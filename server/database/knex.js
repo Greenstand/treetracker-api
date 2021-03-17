d3bbc329bd1b51136faeb6532429a8a88733f44f
Carlos Martins <luacmartins@gmail.com>
2021-03-17 10:17:36 -0600

    fix: knex db connection config

--- server/database/knex.js
+++ server/database/knex.js
@@ -1,19 +1,19 @@
 const expect = require('expect-runtime');
-const log = require("loglevel");
+const log = require('loglevel');
 
 let knexConfig = {
   client: 'pg',
-  debug: process.env.NODE_LOG_LEVEL === "debug"? true:false,
-  process.env.DATABASE_URL,
-  pool: { min:0, max: 100},
-}
+  debug: process.env.NODE_LOG_LEVEL === 'debug' ? true : false,
+  connection: process.env.DATABASE_URL,
+  pool: { min: 0, max: 100 },
+};
 
-log.debug(process.env.DATABASE_SCHEMA)
-if(process.env.DATABASE_SCHEMA){
-  log.info('setting a schema')
-  knexConfig.searchPath = [process.env.DATABASE_SCHEMA]
+log.debug(process.env.DATABASE_SCHEMA);
+if (process.env.DATABASE_SCHEMA) {
+  log.info('setting a schema');
+  knexConfig.searchPath = [process.env.DATABASE_SCHEMA];
 }
-log.debug(knexConfig.searchPath)
+log.debug(knexConfig.searchPath);
 
 const knex = require('knex')(knexConfig);
 
