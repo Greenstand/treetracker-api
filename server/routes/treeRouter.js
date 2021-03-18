506dc4c0b98f9a42757bebdd6376063e373b5094
Carlos Martins <luacmartins@gmail.com>
2021-03-18 13:17:58 -0600

    fix: replace subquery for join

--- server/routes/treeRouter.js
+++ server/routes/treeRouter.js
@@ -8,11 +8,10 @@ treeRouter.get(
   helper.handlerWrapper(async (req, res) => {
     const id = req.query.capture_id;
     //maximum distance in meters between possible matches and tree in query
-    const distance = 6;
+    const distance = req.query.distance || 6;
     const query =
-      'SELECT id, image_url as "imageUrl", latest_capture_id as "latestCaptureId", lat, lon, species_id as "speciesId", morphology, age, status, created_at as "createdAt", updated_at as "updatedAt" FROM treetracker.tree WHERE ST_DWithin(estimated_geographic_location, (SELECT estimated_geographic_location FROM treetracker.capture WHERE id=$1), $2)';
-    const result = await db.query(query, [id, distance]);
-    const data = result.filter((item) => item.id != id);
+      'SELECT id, image_url as "imageUrl", latest_capture_id as "latestCaptureId", lat, lon, species_id as "speciesId", morphology, age, status, created_at as "createdAt", updated_at as "updatedAt" FROM treetracker.tree t1 LEFT JOIN treetracker.capture t2 ON ST_DWithin(t1.estimated_geographic_location, t2.estimated_geographic_location, $2) WHERE t1.id=$1 AND t2.id<>$1';
+    const data = await db.query(query, [id, distance]);
 
     res.status(200).json(data);
   }),
