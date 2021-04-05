0a3e63f3d364cf7922d3c3f4a7bdb2791deba9be
Carlos Martins <luacmartins@gmail.com>
2021-04-05 09:31:51 -0600

    fix: change keys to snake case

--- server/routes/treeRouter.js
+++ server/routes/treeRouter.js
@@ -10,7 +10,7 @@ treeRouter.get(
     //maximum distance in meters between possible matches and tree in query
     const distance = req.query.distance || 6;
     const query =
-      'SELECT id, image_url as "imageUrl", latest_capture_id as "latestCaptureId", lat, lon, species_id as "speciesId", morphology, age, status, created_at as "createdAt", updated_at as "updatedAt" FROM treetracker.tree t1 LEFT JOIN treetracker.capture t2 ON ST_DWithin(t1.estimated_geographic_location, t2.estimated_geographic_location, $2) WHERE t1.id=$1 AND t2.id<>$1';
+      'SELECT id, image_url, latest_capture_id, lat, lon, species_id, morphology, age, status, created_at, updated_at FROM treetracker.tree t1 LEFT JOIN treetracker.capture t2 ON ST_DWithin(t1.estimated_geographic_location, t2.estimated_geographic_location, $2) WHERE t1.id=$1 AND t2.id<>$1';
     const data = await db.query(query, [id, distance]);
 
     res.status(200).json(data);
