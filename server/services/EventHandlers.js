86b525221bf95d5d5176eec8cf91419c670b98fb
Gwynn DP <gwynndp@gmail.com>
2021-04-15 16:58:20 -0700

    refactor: remove unneeded event-handler, handleVerifyCaptureProcessed

--- server/services/EventHandlers.js
+++ server/services/EventHandlers.js
@@ -8,23 +8,6 @@ const {
 const { DomainEvent, receiveEvent } = require('../models/DomainEvent');
 const { applyVerification } = require('../models/Capture');
 
-const handleVerifyCaptureProcessed = async (message) => {
-  const session = new Session(false);
-  const eventRepository = new EventRepository(session);
-  const captureRepository = new CaptureRepository(session);
-  const receive = receiveEvent(eventRepository);
-  const domainEvent = await receive(DomainEvent(message));
-  try {
-    await session.beginTransaction();
-    const handleEvent = applyVerification(captureRepository);
-    handleEvent(message);
-    await eventRepository.update({ id: domainEvent.id, status: 'handled' });
-    await session.commitTransaction();
-  } catch (e) {
-    await session.rollbackTransaction();
-  }
-};
-
 const registerEventHandlers = () => {
   //subscribe('admin-verification', handleVerifyCaptureProcessed);
 };
