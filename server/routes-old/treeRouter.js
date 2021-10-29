3fd75ef88f38b8ebabf584dac04d3f6a58ea4f96
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-10-28 18:19:44 -0700

    fix: lint:fix

--- server/routes-old/treeRouter.js
+++ server/routes-old/treeRouter.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const treeRouter = express.Router();
 const helper = require('./utils');
 const db = require('../database');
@@ -7,7 +8,7 @@ treeRouter.get(
   '/potential_matches',
   helper.handlerWrapper(async (req, res) => {
     const id = req.query.capture_id;
-    //maximum distance in meters between possible matches and tree in query
+    // maximum distance in meters between possible matches and tree in query
     const distance = req.query.distance || 6;
     const query =
       'SELECT id, image_url, latest_capture_id, lat, lon, species_id, morphology, age, status, created_at, updated_at FROM treetracker.tree t1 LEFT JOIN treetracker.capture t2 ON ST_DWithin(t1.estimated_geographic_location, t2.estimated_geographic_location, $2) WHERE t1.id=$1 AND t2.id<>$1';
