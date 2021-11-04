a158e6a95a2752ca2a8548e25ba01188a8ea7c21
deanchen <antlet2004@gmail.com>
2021-11-04 16:24:28 +0800

    fix: broken tests

--- server/models/tree.js
+++ server/models/tree.js
@@ -65,7 +65,8 @@ LEFT JOIN treetracker.tree t1 ON
 	t2.location,
 	:distance) 
 WHERE
-	t2.id = :id 
+  t1.id IS NOT NULL
+  AND	t2.id = :id 
 	AND (t2.tree_id IS NULL OR t2.tree_id <> t1.id )
     `;
 //      'SELECT t1.id, t1.image_url, t1.latest_capture_id, t1.lat, t1.lon, t1.species_id, t1.morphology, t1.age, t1.status, t1.created_at, t1.updated_at FROM treetracker.tree t1 LEFT JOIN treetracker.capture t2 ON ST_DWithin(t1.location, t2.location, :distance) WHERE t1.id= :id AND t2.id<> :id';
