a026d96afe91fe28cb8eb5be19fc63644133e49e
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2021-12-14 01:15:07 +0100

    chore: remove schema from query in util tests

--- server/__tests__/utils.js
+++ server/__tests__/utils.js
@@ -1,26 +1,26 @@
-const {knex} = require("../infra/database/knex");
+const { knex } = require('../infra/database/knex');
 
-function parsePoint(json){
-  for(const key in json){
-    if(json[key].match && json[key].match(/^POINT\(.*\)$/)){
+function parsePoint(json) {
+  for (const key in json) {
+    if (json[key].match && json[key].match(/^POINT\(.*\)$/)) {
       json[key] = knex.raw(`ST_PointFromText('${json[key]}', 4326)`);
     }
   }
-  console.error("j:", json);
+  console.error('j:', json);
   return json;
 }
 
 module.exports = {
-  async addTree(json){
-    await knex("treetracker.tree").insert(parsePoint(json)).returning("*");
+  async addTree(json) {
+    await knex('tree').insert(parsePoint(json)).returning('*');
   },
-  async addCapture(json){
-    await knex("treetracker.capture").insert(parsePoint(json)).returning("*");
+  async addCapture(json) {
+    await knex('capture').insert(parsePoint(json)).returning('*');
   },
-  async delTree(id){
-    await knex("treetracker.tree").where("id", id).del();
+  async delTree(id) {
+    await knex('tree').where('id', id).del();
   },
-  async delCapture(id){
-    await knex("treetracker.capture").where("id", id).del();
+  async delCapture(id) {
+    await knex('capture').where('id', id).del();
   },
-}
+};
