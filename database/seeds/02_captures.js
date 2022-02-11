4cfa0e9015b1dc6c6b9265bc485c35794c730be8
Gwynn DP <gwynndp@gmail.com>
2022-02-10 18:34:24 -0800

    feat: better seed script, all data in one file and it seeds from it in batches

--- database/seeds/02_captures.js
+++ database/seeds/02_captures.js
@@ -1,15 +1,18 @@
-const captures = require('./data/Captures-1.json');
+const captures = require('./data/20220210_Captures.json');
+
 
 exports.seed = function (knex) {
-  // // Deletes ALL existing entries
-  // return (
-  //   knex('capture')
-  //     // .del()
-  //     .then(function () {
-  // // Inserts seed entries
-  return knex('capture').insert(captures);
-  //     })
-  // );
+  // takes the ~ 20,000 captures and splits them into batches of 1000 for seeding
+  const batches = []
+  for (let i = 0; i < captures.length; i += 1000) {
+    const batch = captures.slice(i, i + 1000);
+    batches.push(batch);
+  }
+
+  return Promise.all(batches.map((batch, i) => {
+    // console.log(i, batch.length)
+    return knex('capture').insert(batch)
+  }));
 };
 
 /* EXAMPLE DATA */
@@ -17,30 +20,30 @@ exports.seed = function (knex) {
 // [
 //   {
 //     id: '6ab19f5c-931b-497c-887b-6da6a6572382',
-//     reference_id: 1, // same as id? but set as integer not null
+//     reference_id: 1, // -------- references original integer tree id
 //     tree_id: null,
 //     image_url:
-//       'https://treetracker-production-images.s3.eu-central-1.amazonaws.com/2021.03.17.23.33.54_8.43099064_-13.20422315_de873a43-c21a-40f8-adea-dfc58ef60468_IMG_20210311_123611_1990079067.jpg',
+//       'https://treetracker-production-images.s3.eu-central-1.amazonaws.com/some.jpg',
 //     lat: 8.434773333333334,
 //     lon: -13.201911666666668,
 //     estimated_geographic_location: 'POINT (-13.208441666666666 8.430865)',
-//     estimated_geometric_location:
-//       'SRID=4326;POINT (-13.208441666666666 8.430865)',
+//     estimated_geometric_location: // -------- add 'SRID=4326;' to the front of the geographic_location,
 //     gps_accuracy: null,
 //     grower_id: '85fff895-33c8-4d89-a9b9-60b722beab60',
 //     grower_photo_url: null,
 //     grower_username: 'enexfy0',
 //     planting_organization_id: 'b972cb35-17ba-4c0a-8a63-7487f70d2a26',
-//     device_configuration_id: 'ef01ef97-55a2-4474-9c25-821fe9c5917c', // NOT NULL
+//     device_configuration_id: // -------- NOT NULL
 //     species_id: null,
 //     morphology: null,
 //     age: null,
 //     note: null,
 //     attributes: null,
 //     domain_specific_data: null,
-//     status: 'active', // active/deleted NOT NULL
+//     status: 'active', // -------- active/deleted,  NOT NULL
+//     captured_at: '1/14/2022',
 //     created_at: '1/14/2022',
 //     updated_at: '1/22/2022',
-//     session_id: '6ab19f5c-931b-497c-887b-6da6a6572382', // NOT NULL
+//     session_id:  // -------- NOT NULL
 //   },
 // ];
--- database/seeds/ignore_seed/01_growers.js
+++ database/seeds/ignore_seed/01_growers.js
@@ -3,8 +3,8 @@ const growers = require('../data/20220130-Growers.json');
 exports.seed = function (knex) {
   // return knex('grower_account')
   //   .del()
-  // .then(function () {
-  // Inserts seed entries
-  return knex('grower_account').insert(growers);
+  //   .then(function () {
+      // Inserts seed entries
+      return knex('grower_account').insert(growers);
   // });
 };
