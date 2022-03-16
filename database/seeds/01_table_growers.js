ac3949b687d7713ea03c2aa91d0bb8da96ac5665
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2022-03-16 13:48:56 +0100

    chore: add seed locations to users

--- database/seeds/01_table_growers.js
+++ database/seeds/01_table_growers.js
@@ -1,10 +1,16 @@
 const growers = require('./data/20220130-Growers.json');
+const knex = require('../connection');
+
+const parsePoint = (json) => {
+  const jsonCopy = { ...json };
+  if (jsonCopy.lat && jsonCopy.lon) {
+    jsonCopy.location = knex.raw(
+      `ST_PointFromText('POINT(${jsonCopy.lon} ${jsonCopy.lat})', 4326)`,
+    );
+  }
+  return jsonCopy;
+};
 
 exports.seed = function (knex) {
-  // return knex('grower_account')
-  //   .del()
-  //   .then(function () {
-      // Inserts seed entries
-      return knex('grower_account').insert(growers);
-  // });
+  return knex('grower_account').insert(growers.map((g) => parsePoint(g)));
 };
