c05fb927835065e4b0274ad1b12a7f34d45de9b1
s.zeinolabedin <s.zeinolabedin@hooshmandrisk.ir>
2022-08-10 11:25:00 +0430

    feat: fix already exists error

--- database/migrations/sqls/20220810061329-add-capture-tree-id-down.sql
+++ database/migrations/sqls/20220810061329-add-capture-tree-id-down.sql
@@ -1,1 +1,1 @@
-ALTER TABLE capture DROP COLUMN tree_id;
+ALTER TABLE capture DROP CONSTRAINT capture_tree_id_fkey;
--- database/migrations/sqls/20220810061329-add-capture-tree-id-up.sql
+++ database/migrations/sqls/20220810061329-add-capture-tree-id-up.sql
@@ -1,2 +1,1 @@
-ALTER TABLE capture ADD COLUMN tree_id uuid NOT NULL;
 ALTER TABLE capture ADD CONSTRAINT capture_tree_id_fkey FOREIGN KEY (tree_id) references tree(id);
