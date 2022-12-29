a71037b388d8ac856d79cd95f6b62c8f1cdda9aa
deanchen <antlet2004@gmail.com>
2022-12-29 10:07:40 +0800

    fix: wrong sql

--- database/migrations/sqls/20221229015520-AddCapturedAtIndex-up.sql
+++ database/migrations/sqls/20221229015520-AddCapturedAtIndex-up.sql
@@ -1,3 +1,2 @@
-CREATE INDEX IF NOT EXISTS tree_est_gmtric_loc_idx_gist_gg ON tree USING gist (((estimated_geographic_location)::public.geography));
 CREATE INDEX IF NOT EXISTS capture_captured_at_idx ON capture(captured_at);
 
